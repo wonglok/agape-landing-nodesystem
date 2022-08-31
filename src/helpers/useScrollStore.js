a70e612fc0a70a7056b337504a8d45ec4341924a
Wong Lok <wonglok@users.noreply.github.com>
2022-08-31 15:46:44 +0800

    yo

--- src/helpers/useScrollStore.js
+++ src/helpers/useScrollStore.js
@@ -7,7 +7,7 @@ export const useScrollStore = create((set, get) => {
     setSmooth: (now, dt) => {
       let smooth = get().smooth
 
-      let ll = MathUtils.damp(smooth, now, 0.00215 * 1000, dt)
+      let ll = MathUtils.damp(smooth, now, 1, dt)
 
       set({ smooth: ll, diff: ll - smooth })
     },
