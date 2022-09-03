5bbd177327368c54091d73b953acab6845ea7c7a
Wong Lok <wonglok@users.noreply.github.com>
2022-09-03 22:08:57 +0800

    project editor

--- src/components/projects/ProjectEditor/ProjectEditor.js
+++ src/components/projects/ProjectEditor/ProjectEditor.js
@@ -1,11 +1,24 @@
 import { useGLBEditor } from '@/helpers/useGLBEditor'
+import { useEffect } from 'react'
 
 export function ProjectEditor({}) {
   let project = useGLBEditor((s) => s.currentFolder)
 
+  useEffect(() => {
+    let run = async () => {
+      let handle = project.handle
+      for await (let yo of handle) {
+      }
+    }
+    run()
+  }, [])
   //
   return <div>{project._id}</div>
 }
+
+//
+
+//
 
 //
 
