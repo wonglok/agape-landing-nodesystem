5386b37e81fb4afeebc71f944dcb647c41f87b4f
Wong Lok <wonglok@users.noreply.github.com>
2022-09-08 11:32:29 +0800

    worthy

--- src/components/projects/UseObjectAsPlayer/UseObjectAsPlayer.js
+++ src/components/projects/UseObjectAsPlayer/UseObjectAsPlayer.js
@@ -1,6 +1,8 @@
+import { EffectNodeRuntime } from '@/effectnode/component/EffectNodeRuntime'
 import { useMultiverse } from '@/helpers/useMultiverse'
+import { useAnimations } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { clone } from 'three140/examples/jsm/utils/SkeletonUtils'
 
 export function UseObjectAsPlayer({ glbObject }) {
@@ -9,6 +11,12 @@ export function UseObjectAsPlayer({ glbObject }) {
   //
   let player = useMultiverse((s) => s.player)
   let cloned = clone(glbObject.scene)
+  let anim = useAnimations(glbObject.animations, cloned)
+
+  //
+  useEffect(() => {
+    console.log(anim.names)
+  })
 
   //
   useFrame((st, dt) => {
@@ -18,10 +26,15 @@ export function UseObjectAsPlayer({ glbObject }) {
       ref.current.quaternion.copy(player.quaternion)
     }
   })
+
+  //
   return (
     <group ref={ref}>
       <group rotation={[0, Math.PI, 0]}>
         <primitive key={cloned.uuid} object={cloned} />
+        <EffectNodeRuntime
+          glbObject={{ scene: cloned, animations: glbObject.animations }}
+        ></EffectNodeRuntime>
       </group>
     </group>
   )
