cbafaf7cf75928925833d1bfe4ed7e69cd1d168b
Wong Lok <wonglok@users.noreply.github.com>
2022-09-08 23:04:26 +0800

    protected

--- src/effectnode/store/useEffectNode.js
+++ src/effectnode/store/useEffectNode.js
@@ -2,10 +2,11 @@ import create from 'zustand'
 export const useEffectNode = create((set, get) => {
   //
   return {
-    effectComposer: null,
-    setEffectComposer: (v) => {
-      set({ effectComposer: v })
+    passArray: [],
+    setPassArray: (v) => {
+      set({ passArray: v })
     },
+
     //
   }
 })
