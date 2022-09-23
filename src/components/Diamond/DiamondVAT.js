7ea2a570e6f1b57232bc0dcb66fe2778d9e8b687
Wong Lok <wonglok@users.noreply.github.com>
2022-09-23 13:06:01 +0800

    face

--- src/components/Diamond/DiamondVAT.js
+++ src/components/Diamond/DiamondVAT.js
@@ -37,7 +37,7 @@ export function DiamondVAT() {
             />
           </group>
 
-          <Sphere position={[5, 0, 0]}>
+          {/* <Sphere position={[5, 0, 0]}>
             <meshStandardMaterial
               emissive={'#ff94ff'}
               color={'green'}
@@ -49,7 +49,7 @@ export function DiamondVAT() {
               emissive={'#ffc874'}
               color={'blue'}
             ></meshStandardMaterial>
-          </Sphere>
+          </Sphere> */}
 
           <ambientLight intensity={0.5} />
           <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
