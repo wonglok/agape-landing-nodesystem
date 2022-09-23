fda996a3e0a054c7ba8cd854e8bc0acb85a3f6b1
Wong Lok <wonglok@users.noreply.github.com>
2022-09-23 12:30:10 +0800

    vat2

--- src/pages/vat2.jsx
+++ src/pages/vat2.jsx
@@ -1,4 +1,4 @@
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import { Environment, OrbitControls, useGLTF } from '@react-three/drei'
 import { Canvas, useLoader, useThree } from '@react-three/fiber'
 import { useEffect } from 'react'
 import { Suspense } from 'react'
@@ -32,6 +32,8 @@ export default function Viseme() {
         <Suspense fallback={null}>
           <Content></Content>
         </Suspense>
+
+        <Environment background preset='apartment'></Environment>
         <OrbitControls></OrbitControls>
         <gridHelper args={[100, 100, 0xff0000, 0xffff00]}></gridHelper>
         {/* <OverlayContentAdapter></OverlayContentAdapter> */}
@@ -39,6 +41,7 @@ export default function Viseme() {
     </div>
   )
 }
+/* */
 
 function Content() {
   let glb = useGLTF(`/vat/bricks/rigid.glb`)
@@ -53,54 +56,53 @@ function Content() {
 
     glb.scene.traverse((it) => {
       if (it.geometry) {
-        it.material = new ShaderMaterial({
-          uniforms: {
-            offsetsTex: { value: offsets },
-            normalsTex: { value: normals },
-            progress: { value: 0 },
-          },
-          side: DoubleSide,
-          vertexShader: `
-            uniform sampler2D offsetsTex;
-            uniform sampler2D normalsTex;
-            uniform float progress;
+        // it.material = new ShaderMaterial({
+        //   uniforms: {
+        //     offsetsTex: { value: offsets },
+        //     normalsTex: { value: normals },
+        //     progress: { value: 0 },
+        //   },
+        //   side: DoubleSide,
+        //   vertexShader: `
+        //     uniform sampler2D offsetsTex;
+        //     uniform sampler2D normalsTex;
+        //     uniform float progress;
 
-            varying vec3 vNormals;
-            attribute vec2 uv2;
-            void main (void) {
-              vec4 offsetData = texture2D(offsetsTex,vec2(uv2.x, 1.0 - progress));
-              vec4 normalsData = texture2D(normalsTex,vec2(uv2.x, 1.0 - progress));
+        //     varying vec3 vNormals;
+        //     attribute vec2 uv2;
+        //     void main (void) {
+        //       vec4 offsetData = texture2D(offsetsTex,vec2(uv2.x, 1.0 - progress));
+        //       vec4 normalsData = texture2D(normalsTex,vec2(uv2.x, 1.0 - progress));
 
-              vNormals = (normalsData.xzy);
-              gl_Position = projectionMatrix * modelViewMatrix * (vec4(position + offsetData.xzy, 1.0));
-            }
-          `,
-          fragmentShader: `
-            varying vec3 vNormals;
-            void main (void) {
-                gl_FragColor = vec4(vNormals, 1.0);
-            }
-          `,
-        })
+        //       vNormals = (normalsData.xzy);
+        //       gl_Position = projectionMatrix * modelViewMatrix * (vec4(position + offsetData.xzy, 1.0));
+        //     }
+        //   `,
+        //   fragmentShader: `
+        //     varying vec3 vNormals;
+        //     void main (void) {
+        //         gl_FragColor = vec4(vNormals, 1.0);
+        //     }
+        //   `,
+        // })
 
-        let material = new MeshPhysicalMaterial({})
+        let tasks = []
+        let onLoop = (v) => tasks.push(v)
 
-        material.onBeforeCompile = (shader) => {
-          //
-          //
-          //
-        }
+        it.material = getMat(onLoop, normals, offsets)
 
         let tt
-        let clock = new Clock()
-        let hh = () => {
-          let dt = clock.getDelta()
-          tt = requestAnimationFrame(hh)
-          it.material.uniforms.progress.value += (1 / 250) * 24 * dt
 
-          if (it.material.uniforms.progress.value > 1) {
-            it.material.uniforms.progress.value = 0
-          }
+        let hh = () => {
+          tt = requestAnimationFrame(hh)
+
+          tasks.forEach((t) => t())
+          // let dt = clock.getDelta()
+          // it.material.uniforms.progress.value += (1 / 250) * 24 * dt
+
+          // if (it.material.uniforms.progress.value > 1) {
+          //   it.material.uniforms.progress.value = 0
+          // }
         }
         tt = requestAnimationFrame(hh)
 
@@ -119,6 +121,159 @@ function Content() {
       <primitive object={glb.scene}></primitive>
     </group>
   )
+}
+let getMat = (onLoop, normals, offsets) => {
+  let matt = new MeshPhysicalMaterial({
+    color: new Color('#ffffff'),
+    transparent: true,
+    roughness: 0.0,
+    metalness: 0.5,
+    side: DoubleSide,
+    reflectivity: 1.0,
+    transmission: 1.0,
+    ior: 1.25,
+    thickness: 5,
+  })
+
+  //
+
+  matt.onBeforeCompile = (shader, gl) => {
+    shader.uniforms.time = { value: null }
+
+    let clock = new Clock()
+    onLoop(() => {
+      // let time = window.performance.now() / 1000
+      // shader.uniforms.time.value = time
+
+      let dt = clock.getDelta()
+      shader.uniforms.progress.value += (1 / 250) * 24 * dt
+
+      if (shader.uniforms.progress.value > 1) {
+        shader.uniforms.progress.value = 0
+      }
+    })
+
+    shader.uniforms.offsetsTex = { value: offsets }
+    shader.uniforms.normalsTex = { value: normals }
+    shader.uniforms.progress = { value: 0 }
+
+    shader.vertexShader = shader.vertexShader.replace(
+      `void main() {`,
+      /* glsl */ `
+        uniform sampler2D offsetsTex;
+        uniform sampler2D normalsTex;
+        uniform float progress;
+
+        varying vec3 vNormals;
+        attribute vec2 uv2;
+
+        uniform float time;
+
+        mat3 rotateX(float rad) {
+            float c = cos(rad);
+            float s = sin(rad);
+            return mat3(
+                1.0, 0.0, 0.0,
+                0.0, c, s,
+                0.0, -s, c
+            );
+        }
+        mat3 rotateY(float rad) {
+            float c = cos(rad);
+            float s = sin(rad);
+            return mat3(
+                c, 0.0, -s,
+                0.0, 1.0, 0.0,
+                s, 0.0, c
+            );
+        }
+        mat3 rotateZ(float rad) {
+            float c = cos(rad);
+            float s = sin(rad);
+            return mat3(
+                c, s, 0.0,
+                -s, c, 0.0,
+                0.0, 0.0, 1.0
+            );
+        }
+        mat3 rotateQ (vec3 axis, float rad) {
+            float hr = rad / 2.0;
+            float s = sin( hr );
+            vec4 q = vec4(axis * s, cos( hr ));
+            vec3 q2 = q.xyz + q.xyz;
+            vec3 qq2 = q.xyz * q2;
+            vec2 qx = q.xx * q2.yz;
+            float qy = q.y * q2.z;
+            vec3 qw = q.w * q2.xyz;
+            return mat3(
+                1.0 - (qq2.y + qq2.z),  qx.x - qw.z,            qx.y + qw.y,
+                qx.x + qw.z,            1.0 - (qq2.x + qq2.z),  qy - qw.x,
+                qx.y - qw.y,            qy + qw.x,              1.0 - (qq2.x + qq2.y)
+            );
+        }
+        mat3 calcLookAtMatrix(vec3 origin, vec3 target, float roll) {
+          vec3 rr = vec3(sin(roll), cos(roll), 0.0);
+          vec3 ww = normalize(target - origin);
+          vec3 uu = normalize(cross(ww, rr));
+          vec3 vv = normalize(cross(uu, ww));
+          return mat3(uu, vv, ww);
+        }
+
+
+        void main() {`
+    )
+    //
+
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <begin_vertex>`,
+      /* glsl */ `
+          vec4 offsetData = texture2D(offsetsTex,vec2(uv2.x, 1.0 - progress));
+          vec3 transformed = vec3( position + offsetData.rgb );
+        `
+    )
+
+    shader.vertexShader = shader.vertexShader.replace(
+      `#include <beginnormal_vertex>`,
+      /* glsl */ `
+          // vec3 nPosNormal = normalize(normal);
+
+          // vec4 tt2 = texture2D(tPos, uvinfo.xy);
+          // vec3 tn2 = normalize(tt2.rgb);
+
+          // nPosNormal = rotateX(tn2.r * 3.1415) * nPosNormal;
+          // nPosNormal = rotateY(tn2.g * 3.1415) * nPosNormal;
+          // nPosNormal = rotateZ(tn2.b * 3.1415) * nPosNormal;
+
+            vec4 normalsData = texture2D(normalsTex,vec2(uv2.x, 1.0 - progress));
+
+
+          vec3 objectNormal = normalize(vec3( normalsData ));
+
+          // #ifdef USE_TANGENT
+          //   vec3 objectTangent = vec3( tangent.xyz );
+          // #endif
+          `
+    )
+    shader.fragmentShader = shader.fragmentShader.replace(
+      `void main() {`,
+      `${`
+          // headers //
+        `}\nvoid main() {`
+    )
+    shader.fragmentShader = shader.fragmentShader.replace(
+      `#include <output_fragment>`,
+      `
+        #ifdef OPAQUE
+          diffuseColor.a = 1.0;
+        #endif
+        #ifdef USE_TRANSMISSION
+          diffuseColor.a *= transmissionAlpha + 0.1;
+        #endif
+        gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+        `
+    )
+  }
+  return matt
 }
 
 // let a = /* glsl */ `
