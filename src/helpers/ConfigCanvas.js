048097592d5fb00b02560b3863d0298e1b4a2234
Wong Lok <wonglok@users.noreply.github.com>
2022-09-28 08:01:36 +0800

    save

--- src/helpers/ConfigCanvas.js
+++ src/helpers/ConfigCanvas.js
@@ -3,14 +3,14 @@ import { Color } from 'three'
 import { Core } from './Core'
 
 export const ConfigCanvas = {
-  mode: 'concurrent',
-  shadows: true,
-  gl: { antialias: false },
+  // mode: 'concurrent',
+  // shadows: true,
+  // gl: { antialias: false },
   onCreated: (st) => {
-    st.scene.background = new Color('#ffffff')
+    // st.scene.background = new Color('#ffffff')
     st.gl.physicallyCorrectLights = true
     st.gl.outputEncoding = sRGBEncoding
-    st.gl.shadowMap.enabled = true
+    // st.gl.shadowMap.enabled = true
 
     Core.now.canvas = Core.makeDisposableNode({ name: 'canvas' }).sub
     for (let kn in st) {
