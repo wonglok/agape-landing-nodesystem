e596aacaeb868666e4b2073299c37f8b63d276bc
Wong Lok <wonglok@users.noreply.github.com>
2022-10-11 13:39:58 +0800

    pointer

--- src/components/PointerLockGame/PointerLockGame.js
+++ src/components/PointerLockGame/PointerLockGame.js
@@ -3,6 +3,7 @@ import { ConnectPointerControls } from '@/helpers/ConnectPointerControls'
 import { ConnectSimulationPointer } from '@/helpers/ConnectSimulationPointer'
 import { GLSSR } from '@/helpers/GLSSR'
 import { Player } from '@/helpers/Player'
+import { UIContent } from '@/helpers/UIContent'
 import { Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { Bloom, EffectComposer } from '@react-three/postprocessing'
@@ -18,25 +19,58 @@ export function PointerLockGame() {
 
   //
   return (
-    <Canvas fallback onCreated={(st) => {}}>
-      <EffectComposer multisampling={0}>
-        <Bloom luminanceThreshold={0.0} mipmapBlur></Bloom>
-      </EffectComposer>
+    <>
+      <Canvas
+        gl={{ logarithmicDepthBuffer: true, depth: false }}
+        onCreated={(st) => {}}
+      >
+        <EffectComposer multisampling={2}>
+          <Bloom luminanceThreshold={0.0} mipmapBlur={false}></Bloom>
+        </EffectComposer>
 
-      <Suspense fallback={null}>
-        <group>
-          <ConnectKeyboard></ConnectKeyboard>
-          <ConnectPointerControls></ConnectPointerControls>
-          <ConnectSimulationPointer></ConnectSimulationPointer>
-          <Player visible={false}></Player>
+        <Suspense fallback={null}>
+          <group>
+            <ConnectKeyboard></ConnectKeyboard>
+            <ConnectPointerControls></ConnectPointerControls>
+            <ConnectSimulationPointer></ConnectSimulationPointer>
+            <Player visible={false}></Player>
 
-          <PointerUserControls></PointerUserControls>
+            <PointerUserControls></PointerUserControls>
 
-          <RGBE rgbeURL={rgbeURL}></RGBE>
-          <GameFloor glbURL={gameFloor} enablePostProcessing={true}></GameFloor>
-        </group>
-      </Suspense>
-    </Canvas>
+            <RGBE rgbeURL={rgbeURL}></RGBE>
+            <GameFloor
+              glbURL={gameFloor}
+              enablePostProcessing={true}
+            ></GameFloor>
+          </group>
+        </Suspense>
+      </Canvas>
+
+      <UIContent>
+        <div
+          style={{
+            position: 'fixed',
+            top: 'calc(50% - 1px / 2)',
+            left: 'calc(50% - 25px / 2)',
+            height: '1px',
+            width: `25px`,
+            backdropFilter: 'invert(1)',
+            background: 'transparent',
+          }}
+        ></div>
+        <div
+          style={{
+            position: 'fixed',
+            top: 'calc(50% - 25px / 2)',
+            left: 'calc(50% - 1px / 2)',
+            width: '1px',
+            height: `25px`,
+            backdropFilter: 'invert(1)',
+            background: 'transparent',
+          }}
+        ></div>
+      </UIContent>
+    </>
   )
 }
 
